e51e97883fc3bdee87b17a19ba21983ca502a20a
custompro98 <mitchjoa@gmail.com>
2024-08-27 11:48:00 -0400

    styled page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,32 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        flame: {
+          50: "#fcf5f0",
+          100: "#f8e7dc",
+          200: "#f0cdb8",
+          300: "#e7aa8a",
+          400: "#dc7f5b",
+          500: "#d56440",
+          600: "#c64a30",
+          700: "#a4382a",
+          800: "#843028",
+          900: "#6b2923",
+          950: "#391311",
+        },
+      },
+      fontFamily: {
+        heading: ["SebaldusGotisch"],
+      },
+      boxShadow: {
+        bold: "5px 5px 0 0 rgba(0, 0, 0, 1.0)",
+      },
+    },
+  },
+  plugins: [],
+};
